35ab311a80753562b95233f60173220570f50180
qkhanhpro <dqkpro@gmail.com>
2018-04-26 02:27:33 +0700

    sss

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ var port = process.env.PORT || 8080;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(port, function() {
+app.listen(process.env.PORT || 8080, function() {
     console.log('Our app is running on http://localhost:' + port);
 });
