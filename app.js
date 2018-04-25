8494820b0419d7d8f585473adfaf42c3a6634f41
qkhanhpro <dqkpro@gmail.com>
2018-04-26 02:21:52 +0700

    HerokuJS

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8080;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
-app.listen(80, function () {
-  console.log('Example app listening on port 3000!');
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
 });
