8bbe0604403f1ab0c35a9aa4b5a1d90c278bbbe7
qkhanhpro <dqkpro@gmail.com>
2018-04-26 02:34:55 +0700

    111

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var app = express();
-
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
